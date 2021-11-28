884b4d3221c1568e2688007fa80e2eac50d8821c
“Robertzzel” <robertzel_2001@yahoo.com>
2021-11-28 22:13:06 +0200

    Facut adaugari , modificari

--- WebSite/__init__.py
+++ WebSite/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from databse_tools import Database
+import sqlite3 as sql
 
+DATABASE_NAME = "database.db"
 app = Flask(__name__)
 
 try:
@@ -10,6 +12,136 @@ except:
     print("database deja initializat")
 else:
     print("database initializat")
+
+
+
+def verificare_input(tabel: str, lista_parametri) -> bool:
+    if tabel.upper() == "ELEVI":
+        nr_matricol:str = lista_parametri[0]
+        nume:str = lista_parametri[1]
+        prenume:str = lista_parametri[2]
+        id_facultate:str = lista_parametri[3]
+        id_indrumator:str = lista_parametri[4]
+
+        lista_id_facultate, lista_id_indrumatori = [], []
+        with sql.connect(DATABASE_NAME) as conn:
+            c = conn.cursor()
+            c.execute("SELECT ID_FACULTATE FROM FACULTATI")
+            lista_id_facultate = c.fetchall()
+            c.execute("SELECT ID_PROFESOR FROM PROFESORI")
+            lista_id_indrumatori = c.fetchall()
+            lista_id_indrumatori = [id[0] for id in lista_id_indrumatori]  #procesare lista
+            lista_id_facultate = [id[0] for id in lista_id_facultate]  #procesare_lista
+
+        if nr_matricol.isalpha(): return False
+        if not nume.isalpha(): return False
+        if not prenume.isalpha(): return False
+        if id_facultate.isalpha() or int(id_facultate) not in lista_id_facultate: return False
+        if id_indrumator.isalpha() or int(id_indrumator) not in lista_id_indrumatori: return False
+
+        return True
+
+    elif tabel.upper() == "PROFESORI":
+        id_profesor = lista_parametri[0]
+        id_facultate = lista_parametri[1]
+        nr_materii_predate = lista_parametri[2]
+        salariu = lista_parametri[3]
+
+        lista_id_facultate = []
+        with sql.connect(DATABASE_NAME) as conn:
+            c = conn.cursor()
+            c.execute("SELECT ID_FACULTATE FROM FACULTATI")
+            lista_id_facultate = c.fetchall()
+            lista_id_facultate = [id[0] for id in lista_id_facultate]
+
+        if id_profesor.isalpha(): return False
+        if id_facultate.isalpha() or int(id_facultate) not in lista_id_facultate: return False
+        if nr_materii_predate.isalpha(): return False
+        if salariu.isalpha(): return False
+
+    elif tabel.upper() == "FACULTATI":
+        id_facultate = lista_parametri[0]
+        nr_maxim_profesori = lista_parametri[1]
+        nr_elevi = lista_parametri[2]
+        locatie = lista_parametri[3]
+
+        if id_facultate.isalpha(): return False
+        if nr_maxim_profesori.isalpha(): return False
+        if nr_elevi.isalpha(): return False
+
+        return True
+
+    elif tabel.upper() == "CATALOAGE":
+        nr_matricol = lista_parametri[0]
+        ultima_medie = lista_parametri[1]
+
+        if nr_matricol.isalpha(): return False
+        if (not ultima_medie.isalpha()) and int(ultima_medie) > 0 and int(ultima_medie < 10): return False
+
+        return True
+
+    return False
+
+
+def get_id_type(tabela: str) -> str:
+    id_type = ""
+    if tabela.upper() == "ELEVI":
+        id_type = "NR_MATRICOL"
+    elif tabela.upper() == "PROFESORI":
+        id_type = "ID_PROFESOR"
+    elif tabela.upper() == "FACULTATI":
+        id_type = "ID_FACULTATE"
+    elif tabela.upper() == "CATALOAGE":
+        id_type = "NR_MATRICOL"
+    return id_type
+
+
+def verificare_duplicat_cheie(tabel: str, cheie: str):
+    if tabel.upper() == "ELEVI":
+        lista_nr_matricol = []
+        with sql.connect(DATABASE_NAME) as conn:
+            c = conn.cursor()
+            c.execute("SELECT NR_MATRICOL FROM ELEVI")
+            lista_nr_matricol = c.fetchall()
+            lista_nr_matricol = [id[0] for id in lista_nr_matricol]  # procesare lista
+
+        if int(cheie) in lista_nr_matricol: return False
+        return True
+
+    elif tabel.upper() == "PROFESORI":
+        lista_id_profesor = []
+        with sql.connect(DATABASE_NAME) as conn:
+            c = conn.cursor()
+            c.execute("SELECT ID_PROFESOR FROM PROFESORI")
+            lista_id_profesor = c.fetchall()
+            lista_id_profesor = [id[0] for id in lista_id_profesor]  # procesare lista
+
+        if int(cheie) in lista_id_profesor: return False
+        return True
+
+    elif tabel.upper() == "FACULTATI":
+        lista_id_facultate = []
+        with sql.connect(DATABASE_NAME) as conn:
+            c = conn.cursor()
+            c.execute("SELECT ID_FACULTATE FROM FACULTATI")
+            lista_id_facultate = c.fetchall()
+            lista_id_facultate = [id[0] for id in lista_id_facultate]  #procesare_lista
+
+        if int(cheie) in lista_id_facultate: return False
+        return True
+
+    elif tabel.upper() == "CATALOAGE":
+        lista_nr_matricol = []
+        with sql.connect(DATABASE_NAME) as conn:
+            c = conn.cursor()
+            c.execute("SELECT NR_MATRICOL FROM ELEVI")
+            lista_nr_matricol = c.fetchall()
+            lista_nr_matricol = [id[0] for id in lista_nr_matricol]  # procesare lista
+
+        if int(cheie) in lista_nr_matricol: return False
+        return True
+
+    return False
 
 
 import WebSite.routes
--- WebSite/routes.py
+++ WebSite/routes.py
@@ -1,18 +1,106 @@
-from WebSite import app
-from flask import render_template
+from WebSite import app, DATABASE_NAME, get_id_type, verificare_input, verificare_duplicat_cheie
+from flask import render_template, redirect, request
 import sqlite3 as sql
 
 
 @app.route("/<string:table>")
 def view_data(table: str):
     data: list = []
-    if table.upper() in ("ELEVI", "PROFESORI", "CATALOAGE", "FACULTATI"):
-        with sql.connect("database.db") as conn:
+    if table.upper() in ("ELEVI", "PROFESORI", "CATALOAGE", "FACULTATI"): #verifica ca se cere ceva adevarat din tabela
+        with sql.connect(DATABASE_NAME) as conn:
             c = conn.cursor()
             c.execute(f"select * from {table.capitalize()}")
             data = c.fetchall()
 
     return render_template("view_data.html", type=table, data=data)
+
+
+@app.route("/sterge/<string:tabela>/<int:id>")
+def sterge(tabela: str, id : int):
+
+    id_type = get_id_type(tabela)
+
+    with sql.connect(DATABASE_NAME) as conn:
+        c = conn.cursor()
+        c.execute(f"DELETE FROM {tabela.upper()} WHERE {id_type} = {id}")
+
+    return redirect("/")
+
+
+@app.route("/modifica/<string:tabela>/<int:id>", methods=["GET", "POST"])
+def modifica(tabela: str, id:int):
+
+    if request.method == "GET":
+        id_type = get_id_type(tabela)
+        numele_coloanelor, date_extrase = [], []
+        with sql.connect(DATABASE_NAME) as conn:
+            c = conn.cursor()
+            c.execute(f"SELECT * FROM {tabela.upper()} WHERE {id_type} = {id}")
+            date_extrase = c.fetchone()
+            numele_coloanelor = [description[0] for description in c.description]         # scoate numele coloanelor pentru labeluri
+
+        return render_template('modificare.html', tabela=tabela, headers=numele_coloanelor, data=date_extrase, len=len(numele_coloanelor))
+        # scot prima intrare pentru ca vreau sa sar peste cheia primara care sa nu foe modificata
+
+    elif request.method == "POST":
+
+        key_form = list(request.form.to_dict().keys())
+        valori_form = list(request.form.to_dict().values())
+
+        verif_status = verificare_input(valori_form[-1], valori_form)
+        if verif_status is False:
+            redirect('/')
+        else:
+            #formare string care o sa fie rulat
+            command_string = f"UPDATE {request.form['tabel'].upper()} SET "
+            for i in range(len(key_form)):
+                if valori_form[i] and key_form[i].upper() != 'TABEL':         #sari fieldul cu tabelul la updateuri si verifica daca exista
+                    command_string += f"{key_form[i]} = '{valori_form[i]}' "    #fiecare field de updatat
+                    if i != (len(key_form)-2): command_string += ","          #omite ultima virgula
+
+            command_string += f"WHERE {key_form[0]} = {valori_form[0]}"
+
+            with sql.connect(DATABASE_NAME) as conn:
+                c = conn.cursor()
+                c.execute(command_string)
+
+    return redirect('/')
+
+
+@app.route("/adaugare/<string:tabela>",methods=["GET", "POST"])
+def adaugare(tabela:str):
+    if request.method == "GET":
+        return render_template('adaugare.html', type=tabela)
+
+    if request.method == "POST":
+        key_form = list(request.form.to_dict().keys())
+        valori_form = list(request.form.to_dict().values())
+        marime_form = len(key_form)
+
+        print(key_form)
+        print(valori_form)
+
+        #verificare
+        verif_status = verificare_input(valori_form[-1],valori_form)
+        verif_duplicat = verificare_duplicat_cheie(valori_form[-1],valori_form[0])
+        if verif_status is False or verif_duplicat is False:
+            redirect('/')
+        else:
+            command_string = f"INSERT INTO {valori_form[-1].upper()} VALUES ("
+            for i in range(marime_form):
+                if valori_form[i] and key_form[i].upper() != 'TABEL':
+                    command_string += f"'{valori_form[i]}'"
+                    if i != marime_form - 2:
+                        command_string += ","
+
+            command_string += ")"
+
+            with sql.connect(DATABASE_NAME) as conn:
+                c = conn.cursor()
+                c.execute(command_string)
+
+
+    return redirect('/')
 
 
 @app.route("/")
